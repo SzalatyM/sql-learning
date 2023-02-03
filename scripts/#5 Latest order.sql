11341fbfd43a3369cccbc828d1e2c7f1d8d6f11f
Michał <109979901+SzalatyM@users.noreply.github.com>
2023-02-03 14:31:01 +0100

    Add files via upload

--- scripts/#5 Latest order.sql
+++ scripts/#5 Latest order.sql
@@ -1,5 +1,5 @@
 SELECT users.firstname, users.lastname,orders.orderDateTime
 FROM users LEFT JOIN orders
 ON users.userid = orders.userid
-GROUP BY orders.orderDateTime DESC
+ORDER BY orders.orderDateTime DESC
 LIMIT 1;
